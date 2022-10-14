f52bffc234ce8204b9ac16c89e0d009446c087d7
MadKara <madkara13@gmail.com>
2022-10-14 18:31:36 +0300

    Commit

---
+++ ukrcontent/src/main/java/com/karalash/ukrcontent/UkrcontentApplication.java
@@ -0,0 +1,13 @@
+package com.karalash.ukrcontent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UkrcontentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UkrcontentApplication.class, args);
+	}
+
+}
---
+++ ukrcontent/src/test/java/com/karalash/ukrcontent/UkrcontentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karalash.ukrcontent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UkrcontentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
